dec43ffdb8986c9be7342df01304064f3d53a61e
Maciej <maciej@cosphere.org>
2018-02-17 10:59:18 +0100

    cs-1408: clean up sender

--- lily/conf/settings.py
+++ lily/conf/settings.py
@@ -31,39 +31,6 @@ LILY_AUTHORIZER_CLASS = getattr(
     import_from_string('lily.base.authorizer.Authorizer')
 )
 
-
-# !!!!!!!!!!!!!!!!!
-# FIXME: soon to be removed
-#
-# Contact / Email Sending Settings
-#
-EMAIL_SENDER_FROM = 'notification-no-reply@cosphere.org'
-
-
-# FIXME: the below values would not be needed any more for the
-# sender since all sending will be handled by the mailgun
-EMAIL_SENDER_PASSWORD = 'fZGDSnKJT8q4UySM1'
-
-
-EMAIL_SENDER_MAIL_SERVER_DOMAIN = 'smtp.gmail.com'
-
-
-EMAIL_SENDER_MAIL_SERVER_SSL_PORT = 465
-
-
-CONTACT_SEND_SECRET = (
-    'Th)&PJ3*5PP:#3P7LDhn!VFnS;*>]Zx-y-b3ZD~fAF38RgH,*hCX]w^<L9mm`~e+')
-
-
-CONTACT_SEND_CONFIRMATION_URL = os.environ.get("CONTACT_SEND_CONFIRMATION_URL")
-
-
-CONTACT_CONFIRMATION_CODE_MAX_AGE = 3600 * 24 * 2
-
-
-CONTACT_CONFIRMATION_SALT = 'FGkoFsfs'
-
-
 #
 # Internationalization
 #
