96f716a8640dc70c23ff0f69443dc908e608419a
Maciej <maciej@cosphere.org>
2018-05-09 20:45:42 +0200

    cs-1475: expose other base modules

--- lily/__init__.py
+++ lily/__init__.py
@@ -5,4 +5,4 @@ from .base.meta import *  # noqa
 from .base.input import *  # noqa
 from .base.output import *  # noqa
 from .base.command import *  # noqa
-from .base import *  # noqa
+from .base import name, parsers, serializers  # noqa
