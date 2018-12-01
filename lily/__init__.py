eb09597c41cea8ddffb1f75fb73dfe4c0df52c42
Maciej <maciej@cosphere.org>
2018-12-01 19:17:28 +0100

    add views to the top level imports

--- lily/__init__.py
+++ lily/__init__.py
@@ -5,4 +5,4 @@ from .base.source import *  # noqa
 from .base.input import *  # noqa
 from .base.output import *  # noqa
 from .base.command import *  # noqa
-from .base import name, parsers, serializers  # noqa
+from .base import name, parsers, serializers, views  # noqa
